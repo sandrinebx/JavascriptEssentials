aade568fc364e4e900a617e4cc5dcea61bbf59cf
sandrinebx <sbyu.ecom@gmail.com>
2025-01-13 02:55:32 -0500

    final book management system

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(`+book.index+`)")">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -37,3 +38,7 @@ function clearInputs() {
     document.getElementById('pagesNumber').value = '';
 }
 
+function deleteBook(index){
+    books.splice(index,1);
+    showbooks();
+}
